f0365ebe6f9523975fc5bf4182ab45d3d889bdc0
github lion-king-IT <reo.runningg@gmail.com>
2021-05-29 22:28:45 +0900

    必要な画面追加
    (cherry picked from commit 060f9add70998f54c0c4874452a8fa87c9191789)

--- ui/view/src/main/java/com/reo/running/ui/view/run/GraphGragment.kt
+++ ui/view/src/main/java/com/reo/running/ui/view/run/GraphGragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class RunFragment : Fragment() {
+class GraphGragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
     }
