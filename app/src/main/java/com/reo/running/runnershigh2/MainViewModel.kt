cc67ea46b5dce8b251a4fa4acdfc076943fc0fdf
github lion-king-IT <reo.runningg@gmail.com>
2021-06-10 15:42:17 +0900

    表示・表示をViewModelで定義

--- app/src/main/java/com/reo/running/runnershigh2/MainViewModel.kt
+++ app/src/main/java/com/reo/running/runnershigh2/MainViewModel.kt
@@ -1,6 +1,17 @@
 package com.reo.running.runnershigh2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    private val _isVisibleBottomNavigation = MutableLiveData<Boolean>()
+    val isVisibleBottomNavigation: LiveData<Boolean>
+        get() = _isVisibleBottomNavigation
+
+    init {
+        _isVisibleBottomNavigation.value = false
+    }
+
 }
